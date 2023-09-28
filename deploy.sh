88c5becea7f0faf801bfa3596030a7a170cd4a1d
Halil Agin <halil.agin@gmail.com>
2023-09-28 01:25:43 +0100

    update: postgres vector extension is done with argocd-sync-wave

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,26 @@ llm_package_env_path=$llm_package_root/llm_agent/.env
 llm_package_argocd_app_path=$llm_package_root/argocd/020-argocd-application.yaml
 llm_package_yamls_path=$llm_package_root/yamls
 
-function k(){
-kubectl $@
+
+
+
+init_config(){
+kubectl create ns $llm_package
+kubectl create configmap -n $llm_package llm-0001 --from-env-file=$llm_package_env_path
+#kubectl create -f $llm_package_yamls_path
 }
 
-function deploy(){
-k create ns $llm_package
-k create configmap -n $llm_package llm-0001 --from-env-file=$llm_package_env_path
-k create -f $llm_package_yamls_path
+
+local_deploy(){
+init_config
+kubectl create -f $llm_package_yamls_path
+}
+
+argocd_deploy(){
+init_config
+kubectl create -f $llm_package_argocd_app_path
 }
 
 
-deploy
+local_deploy
+#argocd_deploy
--- llms/llm-0001/scripts/test-db-connection.sh
+++ llms/llm-0001/scripts/test-db-connection.sh
@@ -1,3 +1,3 @@
 set -a
-source llm_agent/.env
+source llms/$1/llm_agent/.env
 psql "$DB_CONNECTION_STRING" -c "select 1 as number;"
